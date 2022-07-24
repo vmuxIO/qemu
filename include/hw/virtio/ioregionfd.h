9c959303a975cf65724931bd3c3f0130d76c0b6b
Sandro-Alessio Gierens <sandro@gierens.de>
2022-07-24 11:20:51 +0200

    Move generic ioregfd init to virtio/ioregionfd.h/c
    The ioregionfd initialization is generic enough to be used for both
    virtio-mmio and virtio-pci, therefore we moved it to the ioregionfd
    files.

--- include/hw/virtio/ioregionfd.h
+++ include/hw/virtio/ioregionfd.h
@@ -2,8 +2,10 @@
 #define IOREGIONFD_H
 
 #include <stdint.h>
-#include <glib.h>
+#include <stdbool.h>
+
 #include "exec/hwaddr.h"
+#include "qemu/typedefs.h"
 #include "io/channel.h"
 
 // TODO maybe it would be prudent to include virtio in the struct and
@@ -27,14 +29,21 @@ typedef void (*mr_write_func)(void *opaque, hwaddr addr, uint64_t value,
 // generic virtio ioregionfd handler function type
 typedef void (*ioregionfd_handler)(void *opaque);
 
-// virtio ioregionfd channel read prototypes
-void virtio_mmio_ioregionfd_handler(void *opaque);
-
 // this function handles ioregionfd reads and write for both virtio-mmio and
 // the different memory regions of virtio-pci
 int virtio_ioregionfd_qio_channel_read(IORegionFD *ioregfd,
                                        mr_read_func read_func,
                                        mr_write_func write_func,
                                        Error **errp);
+
+// setup functions
+int virtio_ioregionfd_channel_setup(IORegionFD *ioregfd,
+                                    Error **errp);
+int virtio_ioregionfd_init(IORegionFD *ioregfd,
+                           gpointer opaque,
+                           MemoryRegion *mr,
+                           uint64_t offset,
+                           uint32_t size,
+                           ioregionfd_handler handler);
 
 #endif /* ifndef IOREGIONFD_H */
